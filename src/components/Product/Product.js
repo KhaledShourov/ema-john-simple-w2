68d2c267081b6d9314949965cc109dc725bb7648
Your Name <you@example.com>
2021-05-09 16:53:12 +0600

    Shop page done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 const Product = (props) => {
-  console.log(props.product)
+  // console.log(props)
   const {img, name, seller, price, stock} = props.product;
   return (
     <div className="product">
@@ -13,6 +15,9 @@ const Product = (props) => {
         <p><small>By:{seller}</small></p>
         <p>Price ${price}</p>
         <p><small>Only {stock} left in stock - Order soon</small></p>
+        <button className="main-btn"
+        onClick={()=>props.handleAddProduct(props.product)}
+        ><FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
       </div>
     </div>
     
